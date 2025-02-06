eedcd09ac83abbfa1b4cad5e30a92bb9dfd543ec
Cody Breitling <codybreitling@gmail.com>
2025-02-06 01:11:23 -0600

    [sc-68] feat: did some cleanup, installed mui, added dashboard dir as landing page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,30 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
 import "./globals.css";
+import { ThemeProvider } from "@mui/material";
+import theme from "./theme";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: ["300", "400", "500", "700"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto",
+});
 
 export const metadata: Metadata = {
   title: "Habit Tracker",
   description: "Track your habits",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout(props: any) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={roboto.variable}>
+        <AppRouterCacheProvider>
+          <ThemeProvider theme={theme}>{props.children}</ThemeProvider>
+        </AppRouterCacheProvider>
+      </body>
     </html>
   );
 }
